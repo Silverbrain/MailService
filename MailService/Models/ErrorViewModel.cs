a45263a146687cb0eae4fa169d353b988bdb99ba
silverbr <sinaataei@outlook.com>
2019-11-20 19:42:27 +0330

    Add project files.

---
+++ MailService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MailService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
