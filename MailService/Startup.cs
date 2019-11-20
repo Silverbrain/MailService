62d2de0b4d1e19b344abeb53d751000c79c798db
silverbr <sinaataei@outlook.com>
2019-11-20 20:39:45 +0330

    Main layout imorted successfully.

--- MailService/Startup.cs
+++ MailService/Startup.cs
@@ -57,7 +57,7 @@ namespace MailService
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Mail}/{action=Inbox}/{id?}");
             });
         }
     }
