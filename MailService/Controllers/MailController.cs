3e35953941ba28a1cbb20faa5a19920822b676f1
silverbr <sinaataei@outlook.com>
2019-11-21 20:25:07 +0330

    NewMail page added. NewMail, sendMail and viewMail actions declaired.

--- MailService/Controllers/MailController.cs
+++ MailService/Controllers/MailController.cs
@@ -2,13 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MailService.Controllers
 {
     public class MailController : Controller
     {
+        [Authorize]
         public IActionResult Inbox()
+        {
+            return View();
+        }
+
+        public IActionResult ViewMail()
+        {
+            return View();
+        }
+
+        public IActionResult NewMail()
+        {
+            return View();
+        }
+
+        public IActionResult SendMail()
         {
             return View();
         }
